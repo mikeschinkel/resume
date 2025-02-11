5eee9205ffb97e0410fed07e30c57aa608a48d7e
Mike Schinkel <mike@newclarity.net>
2025-02-11 03:05:27 -0500

    Tiny change to test webhook deploy

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <head><title>Mike Schinkel's Resume</title></head>
 <body>
 <h1>Mike Schinkel's Resume</h1>
-<h2>Coming soon</h2>
+<h2>Coming Soon</h2>
 In the mean time, you can <a href="mailto:mike+resume@newclarity.net">email me</a>.
 </body>
 </html>
